9160d71092174a5c1f178b75705658605b11acfb
zhan <zhunussov1994@gmail.com>
2019-07-05 16:09:25 +0600

    first commit

---
+++ positive.py
@@ -0,0 +1,12 @@
+list1 = [-10, 21, -4, -45, -66, 93] 
+num = 0
+print("Positive numbers from : ",list1)
+    
+while(num < len(list1)): 
+      
+    
+    if list1[num] >= 0: 
+        
+        print(list1[num], end = " ") 
+       
+    num += 1
